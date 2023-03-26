745904664a4b7ed252948bd97f2f42b9c5e6a030
maxlestage <maxlestage@icloud.com>
2023-03-26 17:52:56 +0200

    UP

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const config = require("./db/config");
 
 // init routes
 const userRoutes = require("./routes/userRoutes");
