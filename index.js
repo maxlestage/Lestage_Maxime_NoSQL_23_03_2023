a45f28a15d5f00d93b15ee2963561cacc356da36
maxlestage <maxlestage@icloud.com>
2023-03-26 17:52:35 +0200

    UP

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(routes);
 app.use("/api/auth", userRoutes);
+
 // app.use("/", auth, commonRoutes);
 app.use("/api/sauces", auth, sauceRoutes);
 
