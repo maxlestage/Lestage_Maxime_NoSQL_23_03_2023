7b495f5eb0b71b8431a264a5482f101a31548362
maxlestage <maxlestage@icloud.com>
2023-03-27 14:38:20 +0200

    Clean OK

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const config = require("./db/config");
 
 // init routes
 const userRoutes = require("./routes/userRoutes");
